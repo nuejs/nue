4664230a895c837da036c44183775fc88d1ce69d
nobkd <44443899+nobkd@users.noreply.github.com>
2024-04-28 01:21:37 +0200

    chore: make arg filtering more robust

--- packages/nuekit/src/cli.js
+++ packages/nuekit/src/cli.js
@@ -26,7 +26,7 @@ export function getArgs(argv) {
   expandArgs(argv.slice(1)).forEach((arg, i) => {
 
     // skip
-    if (arg.endsWith(sep + 'cli.js') || arg.endsWith('/nue') || arg == '--') {
+    if (arg.endsWith(sep + 'cli.js') || arg.endsWith(sep + 'nue') || arg.endsWith(sep + 'nue.cmd') || arg.endsWith(sep + 'nue.ps1') || arg == '--') {
 
     // test suite
     } else if (arg.endsWith('.test.js')) {
