c54ee891fe7424f0d59bd3548da9edc4fc2f2538
Tero Piirainen <tipiirai@gmail.com>
2024-12-08 07:30:37 +0200

    icon and symbol support to <navi> item data

--- packages/nuekit/src/init.js
+++ packages/nuekit/src/init.js
@@ -15,7 +15,7 @@ export async function initNueDir({ dist, is_dev, esbuild, force }) {
   const outdir = join(cwd, dist, '@nue')
 
   // has all latest?
-  const latest = join(outdir, '.rc-1')
+  const latest = join(outdir, '.rc-2')
 
   if (force || !existsSync(latest)) {
     await fs.rm(outdir, { recursive: true, force: true })
