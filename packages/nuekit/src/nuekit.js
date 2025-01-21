e9c8b3e18917f5a7ec89368a074a4c485a980e63
Tero Piirainen <tipiirai@gmail.com>
2025-01-21 09:38:11 +0200

    Small comment update

--- packages/nuekit/src/nuekit.js
+++ packages/nuekit/src/nuekit.js
@@ -15,7 +15,7 @@ import { log, colors, getAppDir, parsePathParts, extendData, toPosix } from './u
 import { renderPage, getSPALayout } from './layout/page.js'
 
 
-// the HTML5 doctype
+// the HTML5 doctype (can/prefer lowercase for consistency)
 const DOCTYPE = '<!doctype html>\n\n'
 
 
