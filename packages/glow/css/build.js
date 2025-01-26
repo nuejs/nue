2221e69dfe52fab7c69b9735b0ed0a56f9ea470b
nobkd <44443899+nobkd@users.noreply.github.com>
2025-01-26 19:42:26 +0100

    feat: minify css with bun

--- packages/glow/css/build.js
+++ packages/glow/css/build.js
@@ -1,21 +1,22 @@
 // Running: bun css/build.js
 import { promises as fs } from 'node:fs'
 
-import { transform, Features } from 'lightningcss'
-
-
-async function minify(names, toname, include = 0) {
+async function minify(names, toname) {
   const raw = []
   for (const name of names) {
     raw.push(await fs.readFile(`css/${name}.css`, 'utf-8'))
   }
 
-  const min = transform({
-    code: Buffer.from(raw.join('\n')),
-    minify: true,
-    include
+  const filename = 'tmp.css'
+  await fs.writeFile(filename, raw.join('\n'))
 
-  }).code?.toString()
+  const min = await (await Bun.build({
+    entrypoints: [filename],
+    minify: true,
+    throw: true,
+  })).outputs[0].text()
+
+  await fs.rm(filename)
 
   const to = `minified/${toname}.css`
   await fs.writeFile(to, min)
@@ -23,6 +24,6 @@ async function minify(names, toname, include = 0) {
 }
 
 
-await minify(['syntax', 'markers'], 'syntax', Features.Nesting)
-await minify(['syntax'], 'syntax.nano', Features.Nesting)
+await minify(['syntax', 'markers'], 'syntax')
+await minify(['syntax'], 'syntax.nano')
 
