3aae931dc3169cfa1273f275ce2a8138e6a49c7c
nobkd <44443899+nobkd@users.noreply.github.com>
2025-01-26 19:33:06 +0100

    feat: use bun css bundler

--- packages/nuekit/src/builder.js
+++ packages/nuekit/src/builder.js
@@ -4,26 +4,37 @@ import { promises as fs } from 'node:fs'
 import { join } from 'node:path'
 
 import { resolve } from 'import-meta-resolve'
-import { Features, bundleAsync } from 'lightningcss'
 
 // don't reuse saved builder when in test mode
 const isTest = process.env.NODE_ENV == 'test'
 
 let jsBuilder
-export async function getBuilder(is_esbuild) {
+export async function getJsBuilder(is_esbuild) {
   if (!isTest && jsBuilder) return jsBuilder
 
   try {
     return jsBuilder = is_esbuild ? await import(resolve('esbuild', `file://${process.cwd()}/`)) : Bun
   } catch {
-    throw 'Bundler not found. Please use Bun or install esbuild'
+    throw 'JS bundler not found. Please use Bun or install esbuild'
+  }
+}
+
+let cssBuilder
+export async function getCssBuilder(is_lcss) {
+  if (!isTest && cssBuilder) return cssBuilder
+
+  try {
+    cssBuilder = is_lcss ? await import(resolve('lightningcss', `file://${process.cwd()}/`)) : Bun
+    return cssBuilder
+  } catch {
+    throw 'CSS bundler not found. Please use Bun >=1.1.34 or install lightningcss'
   }
 }
 
 export async function buildJS(args) {
   const { outdir, toname, minify, bundle } = args
   const is_esbuild = args.esbuild || !process.isBun
-  const builder = await getBuilder(is_esbuild)
+  const builder = await getJsBuilder(is_esbuild)
 
   const opts = {
     external: bundle ? ['../@nue/*', '/@nue/*'] : is_esbuild ? undefined : ['*'],
@@ -53,25 +64,48 @@ export async function buildJS(args) {
 
   } catch ({ errors }) {
     const [err] = errors
-    const error = { text: err.message || err.text, ...(err.location || err.position) }
+    const error = { text: err.message || err.text, ...(err.position || err.location) }
     error.title = error.text.includes('resolve') ? 'Import error' : 'Syntax error'
     delete error.file
     throw error
   }
 }
 
-export async function lightningCSS(filename, minify, opts = {}) {
-  let include = Features.Colors
-  if (!opts.native_css_nesting) include |= Features.Nesting
+export async function buildCSS(filename, minify, opts = {}, lcss) {
+  const is_lcss = lcss || !process.isBun
+  const builder = await getCssBuilder(is_lcss)
+
+  let include
+  if (is_lcss) {
+    include = builder.Features.Colors
+    if (opts.native_css_nesting) include |= builder.Features.Nesting
+  }
 
   try {
-    return (await bundleAsync({ filename, include, minify })).code?.toString()
-  } catch ({ fileName, loc, data }) {
+    if (is_lcss) return (await builder.bundleAsync({
+      filename,
+      include,
+      minify,
+    })).code.toString()
+
+    else return await (await builder.build({
+      entrypoints: [filename],
+      minify,
+      throw: true,
+      experimentalCss: true,
+      // mark basically everything but `.css` as external (TODO: find better solution to this one)
+      external: ['*.svg', '*.png', '*.jpg', '*.jpeg', '*.webp', '*.ico', '*.woff', '*.woff2', '*.ttf', '*.otf'],
+    })).outputs[0].text()
+
+  } catch (e) {
+    // bun aggregate error
+    const [err] = e.errors || [null]
+
     throw {
       title: 'CSS syntax error',
-      lineText: (await fs.readFile(fileName, 'utf-8')).split(/\r\n|\r|\n/)[loc.line - 1],
-      text: data.type,
-      ...loc
+      lineText: err?.position?.lineText || (await fs.readFile(e.fileName, 'utf-8')).split(/\r\n|\r|\n/)[e.loc.line - 1],
+      text: err?.message || e.data.type,
+      ...(err?.position || e.loc),
     }
   }
 }
