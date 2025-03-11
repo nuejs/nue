e02d8f7078ea96c59cce21d621ae204f215acb7c
nobkd <44443899+nobkd@users.noreply.github.com>
2025-03-11 18:15:56 +0100

    re-add fallback to simple title if tooltip unsupported

--- packages/examples/simple-mpa/app/controllers/tooltips.js
+++ packages/examples/simple-mpa/app/controllers/tooltips.js
@@ -9,12 +9,35 @@ tip.id = 'tip'
 // tip hover state
 const timers = []
 let is_shown
+const supportsAnchors = CSS.supports('anchor-name: --tip')
+
+
+function setTooltip(el, title, accesskey) {
+  // on multiple access keys use last
+  const kbd = accesskey && accesskey.split(' ').pop()
+  // build tooltip: html / text
+  const text = [title, kbd && (supportsAnchors ? `<kbd>${kbd}</kbd>` : `[${kbd}]`)]
+    .filter(Boolean).join(' ')
+  
+  // fallback to simple title
+  if (!supportsAnchors) {
+    el.title = text
+    el.dataset.titled = true
+  }
+  // rich tooltip
+  else {
+    tip.innerHTML = text
+  }
+}
 
 
 document.addEventListener('mouseenter', e => {
   const el = e.target
   if (el.nodeType != 1) return
   const dataset = el.dataset
+
+  // forced simple title
+  if (dataset.titled) return
 
   // remove title attribute
   if (el.title) {
@@ -25,25 +48,21 @@ document.addEventListener('mouseenter', e => {
   // no title attribute
   if (!dataset.title) return
 
+  // set tooltip / title
+  setTooltip(el, dataset.title, dataset.accesskey)
+  if (!supportsAnchors) return
+
   // clear timers
   timers.forEach(clearTimeout)
 
-
+  // show tooltip
   timers[0] = setTimeout(_ => {
-    const kbd = dataset.accesskey
-
-    // tip content
-    const html = [dataset.title || el.innerText, kbd && `<kbd>${kbd}</kbd>`]
-    tip.innerHTML = html.filter(Boolean).join(' ')
-
     // position via anchor API
     el.style['anchor-name'] = '--tip'
     tip.classList.toggle('on-bottom', el.closest('header'))
     is_shown = true
 
   }, is_shown ? 50 : 500)
-
-
 }, true)
 
 
