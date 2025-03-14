1e7839147a8436742a713e8cea98508902d43674
nobkd <44443899+nobkd@users.noreply.github.com>
2025-03-14 19:22:19 +0100

    feat: basic ignore support

--- packages/nuekit/src/init.js
+++ packages/nuekit/src/init.js
@@ -30,7 +30,7 @@ async function initDir({ dist, is_dev, esbuild, cwd, srcdir, outdir }) {
   const fromdir = join(srcdir, 'browser')
   const minify = !is_dev
 
-  // simple function to wtite dot
+  // simple function to write dot
   function dot() { process.stdout.write('•') }
 
   // copy from <fromdir>
