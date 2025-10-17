5766149351cfef2f7432e8a44ba645d8ebef0011
Marco Gómez <marco.gomez@monasterios.com.ve>
2025-10-17 08:33:50 -0400

    reverting visual noising changes in create.js

--- packages/nuekit/src/cmd/create.js
+++ packages/nuekit/src/cmd/create.js
@@ -1,9 +1,11 @@
+
 import { mkdir } from 'node:fs/promises'
 import { join } from 'node:path'
 
 export const NAMES = 'blog full minimal spa'.split(' ')
 
 export async function create(name, { dir, baseurl }) {
+
   if (!name) return console.log('❌ USAGE: nue create <template-name>')
 
   if (!NAMES.includes(name)) {
@@ -25,30 +27,30 @@ export async function create(name, { dir, baseurl }) {
     console.log(`   nue\n`)
 
     return true
+
   } catch (error) {
     console.error(`❌ ${error.message}`)
   }
 }
 
+
 export async function getLocalZip(name, dir) {
   const path = join(dir, `${name}.zip`)
-  if (!(await Bun.file(path).exists())) throw new Error(`${path} not found`)
+  if (!await Bun.file(path).exists()) throw new Error(`${path} not found`)
   console.log(`📦 Using local template: ${path}`)
   return Bun.file(path)
 }
 
 // download from github
 //
-export async function fetchZip(
-  name,
-  baseurl = 'https://github.com/nuejs/nue/raw/master/packages/templates'
-) {
+export async function fetchZip(name, baseurl='https://github.com/nuejs/nue/raw/master/packages/templates') {
   const url = `${baseurl}/${name}.zip`
   const resp = await fetch(url)
   if (resp.status != 200) throw new Error(`${url} not found`)
   console.log(`📦 Downloading ${name} template...`)
   return resp
 }
+
 
 // unzip.js
 export async function unzip(dir, zip) {
@@ -68,12 +70,10 @@ export async function unzip(dir, zip) {
       throw new Error(`Unpacking archive failed with exit code ${exitCode}: ${stderr}`)
     }
 
-    // clean up
+  // clean up
   } finally {
     try {
       await Bun.file(filename).delete()
-    } catch (e) {
-      console.info(e)
-    }
+    } catch (e) {console.info(e)}
   }
 }
